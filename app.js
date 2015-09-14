0c32c175e1a43ed73177e13c1f6be5c3bed64f66
Brandon Closson <bclosson@gmail.com>
2015-09-14 17:41:28 -0400

    bootstrap styles added to form and initial functionality in place

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ function($scope) {
 	$scope.test = 'Hello world!';
 
 	$scope.posts = [
-		'post 1',
-		'post 2',
-		'post 3',
-		'post 4',
-		'post 5'
+		{title: 'post 1', upvotes: 2},
+		{title: 'post 2', upvotes: 5},
+		{title: 'post 3', upvotes: 12},
+		{title: 'post 4', upvotes: 4},
+		{title: 'post 5', upvotes: 18}
 	];
+
+	$scope.addPost = function() {
+		if ($scope.title === '' || $scope.title === undefined) { return; }
+		$scope.posts.push({
+			title: $scope.title, 
+			link: $scope.link,
+			upvotes: 4
+		});
+		$scope.title = '';
+	}
+
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	}
 }]);
