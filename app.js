abfefa110d15b7880819d2fe336e17f76c26f305
Brandon Closson <bclosson@gmail.com>
2015-09-14 18:03:22 -0400

    using angular router to load templates

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
 app
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		});
+
+	$urlRouterProvider.otherwise('home');
+}])
 .factory('posts', [function(){
 	var o = {
 		posts: [],
@@ -14,7 +27,7 @@ function($scope, posts) {
 	$scope.test = 'Hello world!';
 
 	$scope.posts = posts.posts;
-	
+
 	$scope.addPost = function() {
 		if ($scope.title === '' || $scope.title === undefined) { return; }
 		$scope.posts.push({
