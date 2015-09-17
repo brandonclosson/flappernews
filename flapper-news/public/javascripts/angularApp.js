984192c16faad76bde58d8856ce5d658a6caffd9
Brandon Closson <bclosson@gmail.com>
2015-09-17 17:42:34 -0400

    added nav controller that exposes auth factory variables

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -208,6 +208,14 @@ function($scope, $state, auth) {
 			$state.go('home');
 		});
 	};
+}])
+.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+	$scope.isLoggedIn = auth.isLoggedIn;
+	$scope.currentUser = auth.currentUser;
+	$scope.logOut = auth.logOut;
 }]);
 
 
